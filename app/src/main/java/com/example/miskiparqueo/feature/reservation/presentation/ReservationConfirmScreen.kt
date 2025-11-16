83eb403eefccbb408bcc2b10682e09833ec619b9
AndresSalva <andres.salvatierra@ucb.edu.bo>
2025-11-16 16:54:03 -0400

    fix: import color and delay for success animation

--- app/src/main/java/com/example/miskiparqueo/feature/reservation/presentation/ReservationConfirmScreen.kt
+++ app/src/main/java/com/example/miskiparqueo/feature/reservation/presentation/ReservationConfirmScreen.kt
@@ -33,13 +33,14 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.miskiparqueo.feature.reservation.presentation.model.ReservationConfirmArgs
+import kotlinx.coroutines.delay
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
 
-@OptIn(ExperimentalMaterial3Api::class)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReservationConfirmScreen(
