230589c4a406893679f84b4de70aa465f0793e30
AndresSalva <andres.salvatierra@ucb.edu.bo>
2025-11-16 16:37:10 -0400

    fix: opt-in material3 api in reservation list

--- app/src/main/java/com/example/miskiparqueo/feature/reservation/presentation/ReservationListScreen.kt
+++ app/src/main/java/com/example/miskiparqueo/feature/reservation/presentation/ReservationListScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -30,6 +31,7 @@ import java.time.format.DateTimeFormatter
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReservationListScreen(
     modifier: Modifier = Modifier,
